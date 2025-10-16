2ad223c63adcce11a4c9910bcf740a0e3a1e7d59
maksymluchak03 <maksym.luchak03@gmail.com>
2025-10-16 20:08:50 +0300

    add authSchemas

---
+++ validationSchemas/authSchemas.ts
@@ -0,0 +1,16 @@
+import { z } from "zod";
+
+export const loginSchema = z.object({
+  email: z.string().min(1, "Email is required").email("Invalid email format"),
+  password: z
+    .string()
+    .min(1, "Password is required")
+    .min(6, "Password must be at least 6 characters"),
+});
+
+export const loginDefaultValues = {
+  email: "",
+  password: "",
+};
+
+export type LoginFormData = z.infer<typeof loginSchema>;
