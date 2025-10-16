7f8247c603f0256814aa85c99da3841d733ff20a
maksymluchak03 <maksym.luchak03@gmail.com>
2025-10-16 20:03:29 +0300

    add constants

---
+++ constants/options.ts
@@ -0,0 +1,12 @@
+export const options = {
+    sortBy: [
+      { label: "Title", value: "title" },
+      { label: "Original title", value: "original_title" },
+      { label: "Popularity", value: "popularity" },
+      { label: "Release date", value: "primary_release_date" },
+      { label: "Revenue", value: "revenue" },
+      { label: "Vote average", value: "vote_average" },
+      { label: "Vote count", value: "vote_count" },
+    ],
+  };
+  
