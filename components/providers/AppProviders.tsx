8849e271c6b0c04e14b67774a88f9f473a8eb241
maksymluchak03 <maksym.luchak03@gmail.com>
2025-10-16 20:08:20 +0300

    add fetch user data

--- components/providers/AppProviders.tsx
+++ components/providers/AppProviders.tsx
@@ -1,27 +1,44 @@
-import "react-native-reanimated";
+import { useAuthStore } from "@/stores/authStore";
 import {
-  DarkTheme,
+  // DarkTheme,
   DefaultTheme,
   ThemeProvider,
 } from "@react-navigation/native";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { useColorScheme } from "@/hooks/use-color-scheme";
+import { useEffect, useState } from "react";
+import "react-native-reanimated";
 import { SafeAreaProvider } from "react-native-safe-area-context";
+// import { useColorScheme } from "react-native";
 
 const queryClient = new QueryClient();
 
-export function AppProviders({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  const colorScheme = useColorScheme();
+export function AppProviders({ children }: { children: React.ReactNode }) {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const loadUser = async () => {
+      try {
+        await useAuthStore.getState().loadUserFromStorage();
+      } catch (error) {
+        console.error("Error loading user:", error);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    loadUser();
+  }, []);
+
+  if (isLoading) {
+    return null;
+  }
 
   return (
     <SafeAreaProvider>
       <QueryClientProvider client={queryClient}>
         <ThemeProvider
-          value={colorScheme === "dark" ? DarkTheme : DefaultTheme}
+          // value={colorScheme === "dark" ? DarkTheme : DefaultTheme}
+          value={DefaultTheme}
         >
           {children}
         </ThemeProvider>
